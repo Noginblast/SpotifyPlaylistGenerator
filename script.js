2096d14f27c9134157b27474c8de317f600eea12
Cade M Forbes <cadeforbes1@gmail.com>
2021-03-24 19:23:22 -0500

    Added Playlist Features

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $( document ).ready(function() {
     let client_id = 'e8714888680a477cb0afc745df9c8030';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
-    let redirect_uri = 'http%3A%2F%2F127.0.0.1%3A5500%2F'; // GitHub Pages URL or whatever your public url to this app is
+    let redirect_uri = 'https%3A%2F%2Fnoginblast.github.io%2FSpotifyPlaylistGenerator%2F'; // GitHub Pages URL or whatever your public url to this app is
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=playlist-modify-private`;
@@ -38,21 +38,15 @@ $( document ).ready(function() {
       //Get the value of the search box
       let raw_search_query = $('#search-text').val();
       let search_query = encodeURI(raw_search_query);
-      let userid;
+      
+      createPlaylist(accessToken);
       // Make Spotify API call
       // Note: We are using the track API endpoint.
       
-      $.ajax({
-        url: `https://api.spotify.com/v1/me`,
-        type: 'GET',
-        headers: {
-            'Authorization' : 'Bearer ' + accessToken
-        },
-        success: function(data) {
-          userid = data.id;
-          createPlaylist(accessToken, userid);
-        }
-      })
+     
+
+
+
       $.ajax({
       url: `https://api.spotify.com/v1/search?q=${search_query}&type=track`,
       type: 'GET',
@@ -83,17 +77,51 @@ $( document ).ready(function() {
     }); // End of search button
   }); // End of document.ready
 
-let createPlaylist = function(accessToken,userid) {
+
+
+  let createPlaylist = function(accessToken) {
+    let userid;
+    let playlistsObject;
+    let playlistExists = false;
+    
+    
     $.ajax({
-      url: `https://api.spotify.com/v1/users/${userid}/playlists`,
-      type: 'POST',
-      headers: {
-          'Authorization': 'Bearer ' + accessToken
-      },
-      data: JSON.stringify({
-        'name': 'Test Playlist',
-        'description': 'THIS IS A TEST',
-        'public': false
+        url: `https://api.spotify.com/v1/me`,
+        type: 'GET',
+        headers: {
+            'Authorization' : 'Bearer ' + accessToken
+        },
+        success: function(data) {
+          userid = data.id;
+
+          $.ajax({
+            url: 'https://api.spotify.com/v1/me/playlists',
+            type: 'GET',
+            headers: {
+                'Authorization' : 'Bearer ' + accessToken
+            },
+            success: function(data){
+              playlistsObject = data;
+            }
+          })
+    
+        
+          $.ajax({
+          url: `https://api.spotify.com/v1/users/${userid}/playlists`,
+          type: 'POST',
+          headers: {
+              'Authorization': 'Bearer ' + accessToken
+          },
+          data: JSON.stringify({
+            'name': 'Test Playlist',
+            'description': 'THIS IS A TEST',
+            'public': false
+            })
+          })
+        }
       })
-      })
+    
+    
+    
+    
   }
